b8d5e51fbe1d69f12e773b64ddffd8b529578911
mateuszstaszkow <mateuszstasz@gmail.com>
2022-03-08 17:04:58 +0100

    feat: [#13] - basic data structure

--- src/mongo_service.py
+++ src/mongo_service.py
@@ -13,4 +13,27 @@ def _connect_mongo(db):
 def read_mongo(db, collection, query={}):
     db = _connect_mongo(db)
     cursor = db[collection].find(query)
-    return pd.DataFrame(list(cursor))
+    data = list(cursor)
+
+    def spread(item):
+        def inner_spread(i):
+
+            return {
+                'date': item['_id'],
+                'total_cost': i['summary'],
+                'city': i['arrival']['city'],
+                'from': i['weekend']['startDay'],
+                'to': i['weekend']['endDay'],
+                **i
+            }
+        return map(inner_spread, item['data'])
+
+    result = list(map(spread, data))
+    ready_data = list()
+    for element in result:
+        for el in element:
+            ready_data.append(el)
+
+    df = pd.DataFrame(ready_data)
+    df.to_csv('../data/data.csv')
+    return df
